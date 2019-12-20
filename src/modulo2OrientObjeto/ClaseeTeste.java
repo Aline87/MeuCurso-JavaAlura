ccc8abee26f8823374090b7ab58b8e9a963a0a3d
Aline Ferraz <aline.santosferraz87@gamil.com>
2019-12-20 17:18:59 -0300

    Iniciando modulo Orientacao a Objeto

---
+++ src/modulo2OrientObjeto/ClaseeTeste.java
@@ -0,0 +1,11 @@
+package modulo2OrientObjeto;
+
+public class ClaseeTeste {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Comecando com modulo 2 Orientacao a Objeto");
+
+	}
+
+}
