7a4dcddcb0d94461d624acb17037ae0bc28f680a
Aline Ferraz <aline.santosferraz87@gamil.com>
2020-01-08 20:28:41 -0300

    aula 04 topico 03

--- src/modulo2OrientObjeto/TestaContaSemCliente.java
+++ src/modulo2OrientObjeto/TestaContaSemCliente.java
@@ -3,15 +3,18 @@ package modulo2OrientObjeto;
 public class TestaContaSemCliente {
 	public static void main(String[] args) {
 		ContaJava2 contaDaMarcela = new ContaJava2();
-		System.out.println(contaDaMarcela.saldo + "Saldo disponivel em conta");
+		
+		System.out.println("Saldo disponivel em conta: " + contaDaMarcela.saldo );
 		contaDaMarcela.titular= new ClienteJava();
-	    contaDaMarcela.titular.nome=" Nome do Cliente : Marcela";
+	    contaDaMarcela.titular.nome="Nome do Cliente : Marcela";
 		System.out.println(contaDaMarcela.titular.nome);
+		
 		contaDaMarcela.titular= new ClienteJava();
-		contaDaMarcela.titular.CPF=" Numero do CPF:346.796.528-88";
+		contaDaMarcela.titular.CPF="Numero do CPF:346.796.528-88";
 		System.out.println(contaDaMarcela.titular.CPF);
+		
 		contaDaMarcela.titular= new ClienteJava();
-		contaDaMarcela.titular.profissao=" Profissao: Programador";
+		contaDaMarcela.titular.profissao="Profissao: Programador";
 		System.out.println(contaDaMarcela.titular.profissao);
 }
 }
